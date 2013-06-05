81fb854fff4e20b98ef8c7f55aea1ad4cb79c2a1
Max Thayer <garbados@gmail.com>
2013-06-05 11:56:19 -0400

    fixed dependencies so build and test work out of the box

--- project.clj
+++ project.clj
@@ -2,13 +2,14 @@
             :description "Call Me Maybe: Network Partitions in Practice"
             :dependencies [[org.clojure/clojure "1.5.0"]
                            [aleph "0.3.0-beta16"]
-                           [com.novemberain/welle "1.6.0-beta2-SNAPSHOT"]
+                           [com.novemberain/welle "1.6.0-beta1"]
                            [com.taoensso/carmine "1.6.0"]
                            [com.novemberain/monger "1.5.0"]
                            [org.clojure/java.jdbc "0.3.0-alpha1"]
                            [korma "0.3.0-RC5"]
                            [postgresql/postgresql "8.4-702.jdbc4"]
                            [org.clojure/tools.cli "0.2.2"]]
-            :profiles {:dev {:dependencies [[midje "1.5.0"]]}}
+            :profiles {:dev {:plugins [[lein-midje "3.0.1"]]
+                             :dependencies [[midje "1.5.0"]]}}
             :main jepsen.bin
             :jvm-opts ["-Xmx128m"])
